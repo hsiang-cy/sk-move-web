8fe2f17072cb7d30a66537ce685b0fcc1f833e9e
hsiang-cy <hsiang2000.ch@gmail.com>
2026-03-08 12:49:49 +0800

    升級至 ts 6, vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import path from 'node:path'
 
 export default defineConfig({
   plugins: [
@@ -10,7 +9,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  resolve: {
-    alias: { '@': path.resolve(import.meta.dirname, './src') },
-  },
+  resolve: { tsconfigPaths: true },
 })
